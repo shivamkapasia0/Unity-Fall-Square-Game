a1eb1c42d2051c4720b81f34672c56ae7cda795f
shivamkapasia0 <kapasiashivam1@outlook.com>
2020-08-26 15:04:47 +0530

    Home Screen Menu Added.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,15 +8,18 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
    
-    public float ballSpeed ; 
-
-
+    public float ballSpeed ;
+    public AudioClip moveSound;
+    public AudioClip pointSound;
+    public AudioClip destroySound;
+    public AudioSource audio;
     public float ballVelocity = 2;
 
     void Start()
     {
-   
-       
+
+        audio = GetComponent<AudioSource>();
+        
         ballSpeed = 1;
     }
 
@@ -31,6 +34,7 @@ public class PlayerController : MonoBehaviour
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began || Input.GetMouseButtonDown(0))
        
         {
+            audio.PlayOneShot(moveSound);
             if (ballSpeed == 1 )
             {
                 ballSpeed = -1;
@@ -45,10 +49,12 @@ public class PlayerController : MonoBehaviour
         else if (transform.position.x >=1.7)
         {
                       ballSpeed = -1;
+                      audio.PlayOneShot(moveSound);
         }
         else   if (transform.position.x <= -1.7)
         {
             ballSpeed = 1;
+            audio.PlayOneShot(moveSound);
         }
         
         // For changing direction of ball on x-axis..
@@ -59,10 +65,12 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
+           
             FindObjectOfType<GameOver>().onGameOver();
             
             // Destroying Player i.e ball
-            Destroy(gameObject);
+            audio.PlayOneShot(destroySound);
+            Destroy(gameObject,0.1f);
             
             
             // Destroying Container of ball i.e line
@@ -70,6 +78,7 @@ public class PlayerController : MonoBehaviour
         }if (other.gameObject.CompareTag("Friend"))
         {
             Destroy(other.gameObject);
+            audio.PlayOneShot(pointSound);
            
            
         }
